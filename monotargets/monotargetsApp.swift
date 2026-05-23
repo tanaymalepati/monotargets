030663e2b64fe5d569f6b52ef71f3216fd090476
tanay <tanayzz@outlook.com>
2026-05-24 01:16:03 +0530

    Initial Commit

---
+++ monotargets/monotargetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  monotargetsApp.swift
+//  monotargets
+//
+//  Created by Tanay M on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct monotargetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
